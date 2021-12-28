67babc67ac95489f928c5fe591234a51713f9381
Gustavo <gustavo7dias@gmail.com>
2021-12-28 08:22:53 -0300

    Update Topbar style

--- src/components/common/Topbar/index.jsx
+++ src/components/common/Topbar/index.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import SocialLinks from "components/common/SocialLinks";
+import styles from "./Topbar.module.css";
 
 const Topbar = () => {
   return (
-    <section id="topbar">
+    <section id={styles.topbar}>
       <div className="container">
-        <div className="contact-info">
+        <div className={styles.contactInfo}>
           <a href="mailto:appmaster@gmail.com">
-            <i className="far fa-envelope sm-icon"></i>
+            <i className="far fa-envelope sm-icon" />
             <p>appmaster@gmail.com</p>
           </a>
           <a href="tel:552140028922">
-            <i className="fas fa-mobile-alt sm-icon"></i>
+            <i className="fas fa-mobile-alt sm-icon" />
             <p>+55 (21)4002-8922</p>
           </a>
         </div>
