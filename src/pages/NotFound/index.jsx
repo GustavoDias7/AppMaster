97b0e9dcf4bbb1cb23d3a8dbe4d06151beda3ea2
Gustavo <gustavo7dias@gmail.com>
2022-01-09 18:17:25 -0300

    Update NotFound page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -2,9 +2,22 @@ import React from "react";
 import NotFoundHead from "components/common/Head/NotFoundHead";
 
 const NotFound = () => {
+  const styles = {
+    position: "absolute",
+    top: "0px",
+    bottom: "0px",
+    left: "0px",
+    right: "0px",
+    margin: "auto",
+    width: "fit-content",
+    height: "min-content",
+  };
   return (
     <>
       <NotFoundHead />
+      <div style={styles}>
+        <h1>AppMaster | Pádina não encontrada</h1>
+      </div>
     </>
   );
 };
